983fbccf6d7b4b6cde5202d280f5bf1c3ea41397
jhonatanserio <94911517+jhonatanserio@users.noreply.github.com>
2022-03-24 10:24:55 -0300

    Update link.js

--- pasta/C29-AP1/link.js
+++ pasta/C29-AP1/link.js
@@ -8,11 +8,15 @@ class Link{
                 bodyA: bodyA.body.bodies[lastlink],
                 pointA: {x: 0,y:0},
                 bodyB: bodyB,
-                pointB: {x:0,Y:0},
+                pointB: {x:0,y:0},
                 length: -10,
                 stiffness:0.01
             }
         );
         World.add(engine.world,this.link);
     }
+// corta a corda da melancia
+  detach(){
+     World.remove(engine.world,this.link)
+  }
 }
